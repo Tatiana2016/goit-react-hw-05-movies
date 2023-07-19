e5b6a70b59eda909c449af508b7a415d4c10e8c1
Tatiana2016 <serarfima@gmail.com>
2023-07-19 23:07:49 +0300

    React.lazy
    виправила для асинхроного завантаження

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Header from './header/Header';
-import Home from './Home';
-import Movies from './Movies';
-import MovieDetails from './MovieDetails';
-import Cast from './Cast';
-import Reviews from './Reviews';
+
+const Home = React.lazy(() => import('./Home'));
+const Movies = React.lazy(() => import('./Movies'));
+const MovieDetails = React.lazy(() => import('./MovieDetails'));
+const Cast = React.lazy(() => import('./Cast'));
+const Reviews = React.lazy(() => import('./Reviews'));
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
